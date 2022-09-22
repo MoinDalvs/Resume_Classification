e3022f75d62b67c85c45fb82cde1b44b2b5ec4df
Moin_Dalvs <moindalvs@gmail.com>
2022-09-22 11:47:07 +0530

    project_by

--- trail_resume.py
+++ trail_resume.py
@@ -192,7 +192,8 @@ def main():
 
     st.subheader("About") 
     st.info("This project is a part of AiVariant Internship")
-    st.markdown('<img align="center" alt="code"  height="180" width="720" src = "https://user-images.githubusercontent.com/99672298/186088252-ef41cefc-cda7-49e8-960d-efbd59d3696c.jpg">\
+    st.sidebar.markdown('#### Project By\n\
+<img align="center" alt="code"  height="180" width="720" src = "https://user-images.githubusercontent.com/99672298/186088252-ef41cefc-cda7-49e8-960d-efbd59d3696c.jpg">\n\
 \
 <table>\
 <tr>\
