4a1be25c7e10714b1966e629de85bc57294fa4c6
Moin_Dalvs <moindalvs@gmail.com>
2022-09-22 10:44:39 +0530

    update

--- trail_resume.py
+++ trail_resume.py
@@ -23,12 +23,12 @@ import aspose.words as aw
 from spacy.matcher import Matcher
 # initialize matcher with a vocab
 matcher = Matcher(nlp.vocab)
-nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
-nltk.download('maxent_ne_chunker')
-nltk.download('words')
-nltk.download('wordnet')
-nltk.download('stopwords')
+# nltk.download('punkt')
+# nltk.download('averaged_perceptron_tagger')
+# nltk.download('maxent_ne_chunker')
+# nltk.download('words')
+# nltk.download('wordnet')
+# nltk.download('stopwords')
 
 mfile = BytesIO(requests.get('https://github.com/MoinDalvs/Resume_Classification/blob/main/model.sav?raw=true').content)
 model = load(mfile)
